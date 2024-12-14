e1e9a3d7a08c501df3a75e7749ee0ab56a6e1fc0
SalvatoreSuella <salvatore@vlkstudio.com>
2024-12-14 18:00:17 +0100

    added post for products

--- apps/backend/server/routes/products/index.post.ts
+++ apps/backend/server/routes/products/index.post.ts
@@ -1,5 +1,29 @@
-export default defineEventHandler(async event => {
-  const id = getRouterParam(event, 'id');
+import { z } from 'zod';
+import { defineEventHandler, readBody } from 'h3';
+import { db } from '~~/src/db';
+import { product } from '~~/src/db/schema';
 
-  return `User profile!`;
+
+const schema = z.array(z.object({
+  name: z.string()
+}));
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const result = schema.safeParse(body);
+
+  if (!result.success) {
+    return {
+      statusCode: 400,
+    };
+  }
+
+  const productNames = result.data;
+
+  await db.insert(product).values(productNames.map(({ name }) => ({ name })));
+
+  return {
+    statusCode: 201
+  };
 });
