63552b42d8f44cc60a653bbdd5049b4acbba3ced
SalvatoreSuella <salvatore@vlkstudio.com>
2024-12-14 20:53:48 +0100

    nitro config

--- apps/backend/nitro.config.ts
+++ apps/backend/nitro.config.ts
@@ -1,5 +1,15 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2024-12-14"
+  compatibilityDate: "2024-12-14",
+  routeRules: {
+    '/api/**': { // Modifica questo percorso in base alle tue API
+      cors: true, // Abilita CORS
+      headers: {
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    },
+  },
 });
