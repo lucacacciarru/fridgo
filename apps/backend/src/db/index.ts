ce714624b1d4200844a80faae94ebde12deb47b6
SalvatoreSuella <salvatore@vlkstudio.com>
2024-12-14 16:28:13 +0100

    fix

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -2,7 +2,8 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 async function main() {
-    const client = postgres(process.env.DATABASE_URL)
+    // Disable prefetch as it is not supported for "Transaction" pool mode 
+    const client = postgres(process.env.DATABASE_URL, { prepare: false })
     const db = drizzle({ client });
 }
 
