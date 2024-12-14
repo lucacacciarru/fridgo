f106771e66c92c9e6cee98173d886945c53a4825
SalvatoreSuella <salvatore@vlkstudio.com>
2024-12-14 17:45:47 +0100

    changed product name

--- apps/backend/server/routes/products/index.get.ts
+++ apps/backend/server/routes/products/index.get.ts
@@ -1,7 +1,7 @@
 import { db } from '~~/src/db';
 
 export default defineEventHandler(async (event) => {
-  const products = await db.query.Product.findMany(); 
+  const products = await db.query.product.findMany(); 
 
   if (products.length === 0) {
     throw createError({ statusCode: 404, statusMessage: 'Products not found' });
--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
-export const Product = pgTable("Product", {
+export const product = pgTable("Product", {
   id: uuid("uuid").primaryKey().defaultRandom().notNull(),
   name: text("text").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
