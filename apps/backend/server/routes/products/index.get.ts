f7d4a9affd460c71ff0ff70b2a802328b990ba2b
SalvatoreSuella <salvatore@vlkstudio.com>
2024-12-14 17:24:58 +0100

    404 no products

--- apps/backend/server/routes/products/index.get.ts
+++ apps/backend/server/routes/products/index.get.ts
@@ -1,5 +1,11 @@
 import { db } from '~~/src/db';
 
-export default defineEventHandler(() => {
-  return db.query.Product.findMany()
+export default defineEventHandler(async (event) => {
+  const products = await db.query.Product.findMany(); 
+
+  if (products.length === 0) {
+    throw createError({ statusCode: 404, statusMessage: 'Products not found' });
+  }
+
+  return products;
 });
